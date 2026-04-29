28d14adcf6cc8f9c8ad82e701311c9ee325b6ab9
Sasho Masatake <smasatak@student.42tokyo.jp>
2026-04-30 05:45:16 +0900

    add ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: smasatak <smasatak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 03:11:59 by smasatak          #+#    #+#             */
-/*   Updated: 2026/04/30 03:24:35 by smasatak         ###   ########.fr       */
+/*   Updated: 2026/04/30 05:44:54 by smasatak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,41 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
+	const char	*big_start;
+	const char	*little_start;
+	size_t		i;
 
+	big_start = big;
+	little_start = little;
+	i = 0;
+	if (*little == '\0')
+		return ((char *)big);
+	while (i < len)
+	{
+		while (*big == *little)
+		{
+			big++;
+			little++;
+			i++;
+			if (*little == '\0')
+				return ((char *)big_start);
+		}
+		little = little_start;
+		big++;
+		big_start ++;
+		i++;
+	}
+	return (NULL);
 }
+
+// int	main(void)
+// {
+// 	const char	str1[] = "Hello";
+// 	const char	str2[] = "el";
+// 	const char	str3[] = "";
+
+// 	printf("%s\n", ft_strnstr(str1, str2, 4));
+// 	printf("%s\n", ft_strnstr(str1, str3, 4));
+// 	printf("%s\n", ft_strnstr(str1, str2, 1));
+// 	return (0);
+// }
