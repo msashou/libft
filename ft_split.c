447fced9e8534e580d336774eb034edbbe8a99ea
Sasho Masatake <smasatak@student.42tokyo.jp>
2026-05-13 20:29:13 +0900

    add ft_split

---
+++ ft_split.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smasatak <smasatak@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/13 15:28:39 by smasatak          #+#    #+#             */
+/*   Updated: 2026/05/13 20:20:26 by smasatak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	count_words(char const *s, char c)
+{
+	int	n;
+
+	n = 0;
+	while (*s)
+	{
+		if (*s != c && *(s + 1) == c || *(s + 1) == '\0')
+			n++;
+		s++;
+	}
+	return (n);
+}
+static char	*extract_word(char const *s, char c)
+{
+	int		len;
+	char	*word;
+	int		i;
+
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	word = malloc(len + 1);
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		word[i] = s[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**res;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	res = malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			res[i++] = extract_word(s, c);
+			while (*s && *s != c)
+				s++;
+		}
+		else
+			s++;
+	}
+	res[i] = NULL;
+	return (res);
+}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: masatakesasho <masatakesasho@student.42    +#+  +:+       +#+        */
+/*   By: smasatak <smasatak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 15:28:19 by masatakesas       #+#    #+#             */
-/*   Updated: 2026/05/13 15:08:34 by masatakesas      ###   ########.fr       */
+/*   Updated: 2026/05/13 15:28:28 by smasatak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,25 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!s1 || !set)
 		return (NULL);
 	start = 0;
-	while (s1[start] && ft_strchr(&s1[start], set))
+	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
 	if (s1[start] == '\0')
 		return (ft_strdup(""));
 	end = ft_strlen(s1) - 1;
-	while (s1[end] && ft_strchr(&s1[end], set))
+	while (s1[end] && ft_strchr(set, s1[end]))
 		end--;
 	return (ft_substr(s1, start, end - start + 1));
 }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int	main()
-{
-	const char	s1[] = "ParisHello 42Tokyo!!Paris";
-	const char	set[] = "Paris";
+// int	main()
+// {
+// 	const char	s1[] = "ParisHello 42Tokyo!!Paris";
+// 	const char	set[] = "Paris";
 
-	printf("Testing ft_strtrim...\n");
-	printf("Before:%s\n", s1);
-	printf("After:%s\n", ft_strtrim(s1, set));
-	return (0);
-}
+// 	printf("Testing ft_strtrim...\n");
+// 	printf("Before:%s\n", s1);
+// 	printf("After:%s\n", ft_strtrim(s1, set));
+// 	return (0);
+// }
